915f926a82294480050598df05346f6f297c43ba
Halemaye <127976303+Halemaye@users.noreply.github.com>
2025-02-13 14:54:12 +0000

    Routing was added between pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeDashboard from './Home';
+import ConnectAWS from './AWSButton';
+import UserLogin from './UserLogin'; 
+import CourseForm from './CourseForm';
+import { Routes, Route } from "react-router-dom";
+
+
 
 // import UserLogin from './UserLogin';
 function App() {
   return (
-    <div className="App">
-      <HomeDashboard />
-    </div>
+    <Routes>
+    <Route path="/" element={<HomeDashboard />} />
+    <Route path="/connectaws" element={<ConnectAWS />} />
+    <Route path="/userlogin" element={<UserLogin />} />
+    <Route path="/courseform" element={<CourseForm />} />
+  </Routes>
   );
 }
 
