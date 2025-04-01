533a3f52a6250a78d129a62b0ab6d253920f28f5
Alexander Judge <judgeal@tcd.ie>
2025-04-01 16:09:17 +0100

    Fixed home test

--- frontend/src/tests/HomeDashboard.test.js
+++ frontend/src/tests/HomeDashboard.test.js
@@ -1,42 +1,46 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import HomeDashboard from '../components/Home/Home';
-
+import { CourseDataProvider } from '../components/Context/CourseDataContext';
 
 describe('HomeDashboard Component', () => {
   it('renders "Home Dashboard" and "Sign In" button', () => {
     render(
-      <MemoryRouter>
-        <HomeDashboard />
-      </MemoryRouter>
+      <CourseDataProvider>
+        <MemoryRouter>
+          <HomeDashboard />
+        </MemoryRouter>
+      </CourseDataProvider>
     );
 
-    //expect(screen.getByText('Home Dashboard')).toBeInTheDocument();
     expect(screen.getByText('Sign In')).toBeInTheDocument();
   });
 
   it('navigates to /login when "Sign In" is clicked', () => {
     render(
-      <MemoryRouter initialEntries={['/']}>
-        <Routes>
-          <Route path="/" element={<HomeDashboard />} />
-          <Route path="/login" element={<div>Login Page</div>} />
-        </Routes>
-      </MemoryRouter>
+      <CourseDataProvider>
+        <MemoryRouter initialEntries={['/']}>
+          <Routes>
+            <Route path="/" element={<HomeDashboard />} />
+            <Route path="/login" element={<div>Login Page</div>} />
+          </Routes>
+        </MemoryRouter>
+      </CourseDataProvider>
     );
 
     const signInButton = screen.getByText('Sign In');
     fireEvent.click(signInButton);
 
-    // Verify if the URL has changed to '/login'
     expect(screen.getByText('Login Page')).toBeInTheDocument();
   });
 
   it('renders links to "Generate Course" and "Connect AWS"', () => {
     render(
-      <MemoryRouter>
-        <HomeDashboard />
-      </MemoryRouter>
+      <CourseDataProvider>
+        <MemoryRouter>
+          <HomeDashboard />
+        </MemoryRouter>
+      </CourseDataProvider>
     );
 
     expect(screen.getByText('Generate Course')).toBeInTheDocument();
