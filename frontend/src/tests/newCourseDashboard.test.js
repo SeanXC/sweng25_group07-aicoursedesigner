2f7b54660176e1f7d7f1869a301d2612bcfe1595
MO1805 <orlowskm@tcd.ie>
2025-04-06 17:08:09 +0100

    work

--- frontend/src/tests/newCourseDashboard.test.js
+++ frontend/src/tests/newCourseDashboard.test.js
@@ -11,7 +11,26 @@ describe('newCourseDashboard Component', () => {
         </MemoryRouter>
       </CourseDataProvider>
     );
-    //expect(screen.getByText('')).toBeInTheDocument();
+    // The three buttons
+    expect(screen.getByText('Translation')).toBeInTheDocument();
+    expect(screen.getByText('Roleplay')).toBeInTheDocument();
+    expect(screen.getByText('Chatbot')).toBeInTheDocument();
 });
+
+  it('navigates to generateRoleplay when "Roleplay Button" is clicked', () => {
+    render(
+      <CourseDataProvider>
+        <MemoryRouter initialEntries={['/']}>
+          <Routes>
+            <Route path="/" element={<courseDashboard />} />
+          </Routes>
+        </MemoryRouter>
+      </CourseDataProvider>
+    );
+    const roleplayButton = screen.getByText('Roleplay');
+    fireEvent.click(roleplayButton);
+
+    expect(screen.getByText('Generate Roleplay')).toBeInTheDocument();
+  });
 
 });
