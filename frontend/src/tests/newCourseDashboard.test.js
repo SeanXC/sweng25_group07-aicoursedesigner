c4217d4747a3b50b8fea29d6cad71a0280c9b5e3
MO1805 <orlowskm@tcd.ie>
2025-04-06 20:19:11 +0100

    renamed newCouseDashboard + simplified test

--- frontend/src/tests/newCourseDashboard.test.js
+++ frontend/src/tests/newCourseDashboard.test.js
@@ -1,9 +1,11 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
-import { newCourseDashboard } from 'componenets/newCourseDashboard/newCourseDashboard';
+import  newCourseDashboard  from 'componenets/newCourseDashboard/newCourseDashboard';
+import { CourseDataProvider } from '../components/Context/CourseDataContext';
 
 describe('newCourseDashboard Component', () => {
-  it('renders "new Course Dashboard"', () => {
+
+  it('render the new Course Dashboard and the 3 buttons', () => {
     render(
       <CourseDataProvider>
         <MemoryRouter>
@@ -15,22 +17,5 @@ describe('newCourseDashboard Component', () => {
     expect(screen.getByText('Translation')).toBeInTheDocument();
     expect(screen.getByText('Roleplay')).toBeInTheDocument();
     expect(screen.getByText('Chatbot')).toBeInTheDocument();
-});
-
-  it('navigates to generateRoleplay when "Roleplay Button" is clicked', () => {
-    render(
-      <CourseDataProvider>
-        <MemoryRouter initialEntries={['/']}>
-          <Routes>
-            <Route path="/" element={<courseDashboard />} />
-          </Routes>
-        </MemoryRouter>
-      </CourseDataProvider>
-    );
-    const roleplayButton = screen.getByText('Roleplay');
-    fireEvent.click(roleplayButton);
-
-    expect(screen.getByText('Generate Roleplay')).toBeInTheDocument();
   });
-
 });
